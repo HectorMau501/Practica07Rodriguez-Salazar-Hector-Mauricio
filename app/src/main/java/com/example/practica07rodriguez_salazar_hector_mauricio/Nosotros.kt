fe37ce7652754ecd56760ddf5bbfb012e615659f
HectorMau501 <mauhector7@gmail.com>
2023-09-29 09:06:58 -0600

    Ya casi acabe el diseño

---
+++ app/src/main/java/com/example/practica07rodriguez_salazar_hector_mauricio/Nosotros.kt
@@ -0,0 +1,11 @@
+package com.example.practica07rodriguez_salazar_hector_mauricio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Nosotros : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nosotros)
+    }
+}
